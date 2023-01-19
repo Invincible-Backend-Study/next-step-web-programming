d079d8d480ced40a26d33c2e2e7d21edb79291ce
HiiWee <wpdlzhf159@gmail.com>
2023-01-19 23:55:19 +0900

    test: request line 정상 읽어오기 테스트 추가

--- web-application-server-gradle/src/main/java/utils/dto/RequestLine.java
+++ web-application-server-gradle/src/main/java/utils/dto/RequestLine.java
@@ -1,5 +1,6 @@
 package utils.dto;
 
+import java.util.Objects;
 import utils.enums.HttpMethod;
 
 public class RequestLine {
@@ -20,5 +21,23 @@ public class RequestLine {
 
     public String getUri() {
         return requestUri;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        RequestLine that = (RequestLine) o;
+        return httpMethod == that.httpMethod && requestUri.equals(that.requestUri) && httpVersion.equals(
+                that.httpVersion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(httpMethod, requestUri, httpVersion);
     }
 }
