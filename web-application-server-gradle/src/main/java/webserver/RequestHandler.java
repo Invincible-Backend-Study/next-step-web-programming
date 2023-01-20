fdc39bbc4b49c8bccafee757f8e5702c4bcfc00a
wnsgh12s <junho1273@gmail.com>
2023-01-20 11:53:53 +0900

    Feat: post 요청 기능

--- web-application-server-gradle/src/main/java/webserver/RequestHandler.java
+++ web-application-server-gradle/src/main/java/webserver/RequestHandler.java
@@ -10,10 +10,10 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import model.User;
 import org.slf4j.Logger;
@@ -34,25 +34,37 @@ public class RequestHandler extends Thread {
     public void run() {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
-
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            byte[] body = null;
-            BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
             String line = reader.readLine();
             if (line == null) {
                 return;
             }
             String url = Arrays.asList(line.split(" ")).get(1);
+
+            HashMap<String, String> headers = new HashMap();
+            while (!"".equals(line)) {
+                line = reader.readLine();
+                List<String> splited = Arrays.asList(line.split(":"));
+                if (splited.size() >= 2) {
+                    headers.put(splited.get(0), splited.get(1));
+                }
+            }
             DataOutputStream dos = new DataOutputStream(out);
+
             if (url.startsWith("/user/create")) {
-                int index = url.indexOf("?");
-                String param = url.substring(index + 1);
-                Map<String, String> queryString = HttpRequestUtils.parseQueryString(param);
+                // int index = url.indexOf("?");
+                //String bodyData = url.substring(index + 1);
+                String bodyData = IOUtils.readData(reader, Integer.parseInt(headers.get("Content-Length").trim()));
+                Map<String, String> queryString = HttpRequestUtils.parseQueryString(bodyData);
                 User user = new User(queryString.get("userId"), queryString.get("password"), queryString.get("name"),
                         queryString.get("email"));
                 url = "/index.html";
+
             }
-            body = Files.readAllBytes(Paths.get(new File("./web-application-server-gradle/webapp").toPath() + url));
+
+            byte[] body = Files.readAllBytes(Paths.get(new File("./web-application-server-gradle/webapp").toPath() + url));
+
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
