02225b6df4101f8f740d90042a9b91a0b9098100
HiiWee <wpdlzhf159@gmail.com>
2023-01-21 05:37:45 +0900

    feat: 로그인 성공시 쿠키 생성 및 리다이렉트 기능 추가

--- web-application-server-gradle/src/main/java/customwebserver/http/HttpResponse.java
+++ web-application-server-gradle/src/main/java/customwebserver/http/HttpResponse.java
@@ -5,12 +5,16 @@ import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HttpResponse {
     private static final Logger log = LoggerFactory.getLogger(HttpResponse.class);
     private final DataOutputStream dos;
+    private String forwardUri;
+    private final Map<String, Object> cookies = new HashMap<>();
 
     public HttpResponse(final OutputStream out) {
         dos = new DataOutputStream(out);
@@ -33,23 +37,46 @@ public class HttpResponse {
         responseBody(dos, fileBytes);
     }
 
+    public void setCookie(final String cookieKey, final Object value) {
+        cookies.put(cookieKey, value);
+    }
+
+    private String getAllCookieMessage() {
+        StringBuilder cookieMessage = new StringBuilder();
+        cookieMessage.append("Set-Cookie: ");
+        if (!cookies.isEmpty()) {
+            cookies.keySet()
+                    .forEach(key -> cookieMessage.append(key)
+                            .append(":")
+                            .append(cookies.get(key))
+                            .append("; "));
+            return cookieMessage.delete(cookieMessage.length() - 2, cookieMessage.length())
+                    .append("\r\n")
+                    .toString();
+        }
+        return "";
+    }
+
     private void response302Header(final DataOutputStream dos, final int lengthOfBodyContent, final String path) {
         try {
             dos.writeBytes("HTTP/1.1 302 Found \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("Location: " + "http://localhost:8080" + path);
+            dos.writeBytes(getAllCookieMessage());
+            dos.writeBytes("Location: http://localhost:8080" + path + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
+
     private void response200Header(final DataOutputStream dos, final int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes(getAllCookieMessage());
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
