0087df91fd62b3e8ef7fc88bf96d104257ec7b51
HiiWee <wpdlzhf159@gmail.com>
2023-01-21 00:05:33 +0900

    feat: 프론트 컨트롤러와 HttpRequest간 기능 분리

--- web-application-server-gradle/src/main/java/controller/FrontController.java
+++ web-application-server-gradle/src/main/java/controller/FrontController.java
@@ -1,50 +1,54 @@
 package controller;
 
-import java.io.BufferedReader;
+import controller.http.HttpRequest;
+import controller.http.HttpResponse;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import utils.HttpRequestUtils;
-import utils.IOUtils;
-import utils.dto.Pair;
-import utils.dto.RequestLine;
-import webserver.RequestHandler;
+import utils.enums.HttpMethod;
 
 public class FrontController {
-    public static final String EMPTY = "";
-    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+    private static final Logger log = LoggerFactory.getLogger(FrontController.class);
+    private static final Map<String, Object> handlerMapping = new HashMap<>();
 
-    private final BufferedReader httpRequest;
-    private RequestLine requestLine;
-    private Map<String, String> requestHeaderKeyValue;
-
-    public FrontController(final InputStream in) throws IOException {
-        httpRequest = new BufferedReader(new InputStreamReader(in));
-        requestLine = HttpRequestUtils.parseRequestLine(httpRequest.readLine());
-        requestHeaderKeyValue = parseHttpRequestHeaderKeyValue(httpRequest);
-
-
+    static {
+        handlerMapping.put("/user/create", new UserController());
     }
 
-    private Map<String, String> parseHttpRequestHeaderKeyValue(final BufferedReader httpRequest) throws IOException {
-        String header = httpRequest.readLine();
-        List<Pair> headerPairs = new ArrayList<>();
-        while (!header.equals(EMPTY)) {
-            log.info("header={}", header);
-            headerPairs.add(HttpRequestUtils.parseHeader(header));
-            header = httpRequest.readLine();
+    private final HttpRequest httpRequest;
+    private final HttpResponse httpResponse;
+
+    public FrontController(final HttpRequest httpRequest, final HttpResponse httpResponse) throws IOException {
+        this.httpRequest = httpRequest;
+        this.httpResponse = httpResponse;
+        requestDispatch();
+    }
+
+    /**
+     * 1. 컨트롤러가 존재하는지 체크
+     * 2. 정적 파일 체크
+     */
+    private void requestDispatch() throws IOException {
+        Controller requestController = (Controller) handlerMapping.get(httpRequest.getRequestUri());
+        if (requestController == null) {
+            byte[] fileBytes = Files.readAllBytes(new File("./webapp" + httpRequest.getRequestUri()).toPath());
+            httpResponse.successStaticUri(fileBytes);
+            return;
         }
-        return headerPairs.stream()
-                .collect(Collectors.toMap(Pair::getKey, Pair::getValue));
+        if (httpRequest.containMethod(HttpMethod.GET)) {
+            boolean success = requestController.doGet(httpRequest, httpResponse);
+            response(success);
+        }
     }
 
-    public RequestLine getRequestLine() {
-        return requestLine;
+    private void response(final boolean success) {
+        if (success) {
+            httpResponse.successMappingUri();
+        }
     }
+
 }
