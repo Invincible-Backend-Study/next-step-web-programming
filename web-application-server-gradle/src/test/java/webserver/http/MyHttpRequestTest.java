a48a2570772eab6dc21c04e610487dfd86c1ed96
Basak <chgy2131@naver.com>
2023-01-26 00:46:19 +0900

    test(MyHttpRequest): POST 메소드 사용시 parameters를 강제로 url에 넣는 경우 테스트

--- web-application-server-gradle/src/test/java/webserver/http/MyHttpRequestTest.java
+++ web-application-server-gradle/src/test/java/webserver/http/MyHttpRequestTest.java
@@ -30,6 +30,17 @@ class MyHttpRequestTest {
         assertEquals("POST", myHttpRequest.getHttpMethod());
         assertEquals("/user/create", myHttpRequest.getRequestPath());
         assertEquals("keep-alive", myHttpRequest.getHeader("Connection"));
-        assertEquals("myid", myHttpRequest.getParameter("userId"));
+        assertEquals("myii", myHttpRequest.getParameter("userId"));
+    }
+
+    @Test
+    public void request_POST_paramsInUrl() throws Exception {
+        InputStream in = new FileInputStream(new File(testDirectory + "Http_POST_paramsInUrl.http"));
+        MyHttpRequest myHttpRequest = new MyHttpRequest(in);
+
+        assertEquals("POST", myHttpRequest.getHttpMethod());
+        assertEquals("/user/create", myHttpRequest.getRequestPath());
+        assertEquals("keep-alive", myHttpRequest.getHeader("Connection"));
+        assertEquals("myjj", myHttpRequest.getParameter("userId"));
     }
 }
