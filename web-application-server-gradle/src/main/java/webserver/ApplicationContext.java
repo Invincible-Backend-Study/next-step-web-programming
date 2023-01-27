072bdff7c68fe591de05d223267e7a895a7dc023
JaehongDev <println@kakao.com>
2023-01-27 23:35:23 +0900

    feature(ApplicationContext): http request 와 response를 처리하는 공통 로직 분리

---
+++ web-application-server-gradle/src/main/java/webserver/ApplicationContext.java
@@ -0,0 +1,33 @@
+package webserver;
+
+import common.BasicController;
+import domain.user.ui.RegisterUserController;
+import http.request.HttpRequest;
+import http.response.HttpResponse;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class ApplicationContext {
+    private static final Map<String, BasicController> previousMappingController = new LinkedHashMap<>();
+
+    static {
+        previousMappingController.put("/user/create", new RegisterUserController());
+        //previousMappingController.put("/user")
+    }
+
+    public static void process(HttpRequest request, HttpResponse response) throws IOException {
+        var requestPath = request.getStartLine().getUrl();
+
+        var findController = Optional.ofNullable(previousMappingController.getOrDefault(requestPath, null));
+
+        findController.orElseGet(() -> ((httpRequest, httpResponse) -> {
+            // handle error or html file
+            response.write("HTTP/1.1 200 OK ")
+                    .writeFile(requestPath);
+        })).process(request, response);
+
+        response.send();
+    }
+}
