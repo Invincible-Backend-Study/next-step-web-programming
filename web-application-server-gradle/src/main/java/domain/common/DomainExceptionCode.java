2b8edb9e0efa5bd82d1d21a23ecc882c6293cee3
JaehongDev <println@kakao.com>
2023-01-28 02:29:39 +0900

    feature(DomainException): 도메인 예외정책 추가

--- web-application-server-gradle/src/main/java/domain/common/DomainExceptionCode.java
+++ web-application-server-gradle/src/main/java/domain/common/DomainExceptionCode.java
@@ -10,10 +10,13 @@ import lombok.RequiredArgsConstructor;
 public enum DomainExceptionCode {
     USER(100),
     PREVIOUS_SIGN_UP_USER_ID(USER.code + 1, "이미 존재하는 아이디로 회원가입을 할 수 없습니다 [input = %s]"),
-    USER_ID_IS_NULL(USER.code + 2, "아이디가 빈칸 혹은 존재 하지 않습니다.");
+    USER_ID_IS_NULL(USER.code + 2, "아이디가 빈칸 혹은 존재하지 않습니다."),
+    USER_PASSWORD_IS_NULL(USER.code + 3, "사용자 비밀번호가 빈칸이거나 존재하지 않습니다."),
+    USER_NAME_IS_NULL(USER.code + 4, "사용자 이름이 빈칸이거나 존재하지 않습니다."),
+    USER_EMAIL_IS_NULL(USER.code + 5, "사용자 이메일이 빈칸이거나 존재하지 않습니다.");
 
 
-    private static final String ERROR_FORMAT = "[ERROR] [CODE: %i] message:%s";
+    private static final String ERROR_FORMAT = "[ERROR] [CODE: %s] message:%s";
     private final int code;
     private final String message;
 
--- web-application-server-gradle/src/main/java/domain/user/entity/ValidateUserPolicy.java
+++ web-application-server-gradle/src/main/java/domain/user/entity/ValidateUserPolicy.java
@@ -4,8 +4,30 @@ import domain.common.DomainExceptionCode;
 
 public class ValidateUserPolicy {
     public static void validate(User user) {
-
         validateUserId(user.getUserId());
+        validateUsername(user.getName());
+        validateUserPassword(user.getPassword());
+        validateUserEmail(user.getEmail());
+    }
+
+    private static void validateUserEmail(String email) {
+        if (email == null) {
+            throw DomainExceptionCode.USER_EMAIL_IS_NULL.createError();
+        }
+    }
+
+    private static void validateUserPassword(String name) {
+        if (name == null) {
+            throw DomainExceptionCode.USER_NAME_IS_NULL.createError();
+        }
+
+    }
+
+    private static void validateUsername(String password) {
+        if (password == null) {
+            throw DomainExceptionCode.USER_PASSWORD_IS_NULL.createError();
+        }
+
     }
 
     private static void validateUserId(String userId) {
