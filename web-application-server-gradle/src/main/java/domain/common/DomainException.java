4f4ee53478bf35577cdfc944656e668962d4f48a
JaehongDev <println@kakao.com>
2023-01-28 01:50:12 +0900

    feature (DomainException) : 전체 도메인에 대한 예외 및 코드 구현

--- web-application-server-gradle/src/main/java/domain/common/DomainException.java
+++ web-application-server-gradle/src/main/java/domain/common/DomainException.java
@@ -1,4 +1,22 @@
 package domain.common;
 
-public class DomainException {
+
+import java.util.Optional;
+
+public class DomainException extends RuntimeException {
+    private final String redirectUrl;
+
+    public DomainException(String message, String redirectUrl) {
+        super(message);
+        this.redirectUrl = redirectUrl;
+    }
+
+    public DomainException(String message) {
+        super(message);
+        this.redirectUrl = null;
+    }
+
+    public Optional<String> getRedirectUrl() {
+        return Optional.ofNullable(redirectUrl);
+    }
 }
--- web-application-server-gradle/src/main/java/domain/common/DomainExceptionCode.java
+++ web-application-server-gradle/src/main/java/domain/common/DomainExceptionCode.java
@@ -8,8 +8,26 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum DomainExceptionCode {
-    USER(100);
+    USER(100),
+    PREVIOUS_SIGN_UP_USER_ID(USER.code + 1, "이미 존재하는 아이디로 회원가입을 할 수 없습니다 [input = %s]"),
+    USER_ID_IS_NULL(USER.code + 2, "아이디가 빈칸 혹은 존재 하지 않습니다.");
 
 
+    private static final String ERROR_FORMAT = "[ERROR] [CODE: %i] message:%s";
     private final int code;
+    private final String message;
+
+    DomainExceptionCode(int code) {
+        this.code = code;
+        this.message = "";
+    }
+
+    public DomainException createError(String redirectUrl, Object... args) {
+        var errorMessage = String.format(message, args);
+        return new DomainException(String.format(ERROR_FORMAT, code, errorMessage), redirectUrl);
+    }
+
+    public DomainException createError() {
+        return new DomainException(String.format(ERROR_FORMAT, code, message));
+    }
 }
