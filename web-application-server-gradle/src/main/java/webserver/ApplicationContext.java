90c05e7b50fa8982e51556164186ef17d489fe9f
JaehongDev <println@kakao.com>
2023-01-28 02:23:36 +0900

    refactor(PreviousMappingController) : 미리 매핑하는 컨틀롤러 클래스 분리

--- web-application-server-gradle/src/main/java/webserver/ApplicationContext.java
+++ web-application-server-gradle/src/main/java/webserver/ApplicationContext.java
@@ -1,37 +1,23 @@
 package webserver;
 
-import common.BasicController;
-import domain.user.ui.FindUserController;
-import domain.user.ui.RegisterUserController;
-import domain.user.ui.UserLoginController;
+import common.PreviousMappingController;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
 import java.io.IOException;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Optional;
 
 public class ApplicationContext {
-    private static final Map<String, BasicController> previousMappingController = new LinkedHashMap<>();
-
-    static {
-        previousMappingController.put("/user/create", new RegisterUserController());
-        previousMappingController.put("/user/login", new UserLoginController());
-        previousMappingController.put("/user/list", new FindUserController());
-        //previousMappingController.put("/user")
-    }
+    private static final PreviousMappingController mappingController = new PreviousMappingController();
 
     public static void process(HttpRequest request, HttpResponse response) throws IOException {
-        var requestPath = request.getStartLine().getUrl();
+        // request interceptor 필요한 경우
 
-        var findController = Optional.ofNullable(previousMappingController.getOrDefault(requestPath, null));
+        mappingController.findControllerBy(request.getRequestPath())
+                .orElseGet(mappingController::getResourceHandleController)// handle error or html file
+                .process(request, response);
 
-        findController.orElseGet(() -> ((httpRequest, httpResponse) -> {
-            // handle error or html file
-            response.write("HTTP/1.1 200 OK ")
-                    .writeFile(requestPath);
-        })).process(request, response);
-
+        // response interceptor 필요한 경우
         response.send();
     }
+
+
 }
