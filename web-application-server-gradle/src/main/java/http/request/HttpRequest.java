7ae7e12f7a0bcb8900c8023a1ab6ef91e8cbb588
JaehongDev <println@kakao.com>
2023-01-28 02:23:56 +0900

    refactor(HttpRequest) : http request 인터페이스에서 추상 클래스로 분리

--- web-application-server-gradle/src/main/java/http/request/HttpRequest.java
+++ web-application-server-gradle/src/main/java/http/request/HttpRequest.java
@@ -4,18 +4,37 @@ import http.request.header.HttpHeader;
 import http.request.startline.HttpRequestStartLine;
 import java.util.Map;
 import java.util.Optional;
+import util.HttpRequestUtils;
 
-public interface HttpRequest {
-    // 요청 타입
-    // 요청 url
-    // 쿼리 스트링
-    HttpRequestStartLine getStartLine();
 
-    HttpHeader getHttpHeader();
+public abstract class HttpRequest {
 
-    Map<String, String> getParameters();
+    private final HttpRequestStartLine httpRequestStartLine;
+    private final HttpHeader httpHeaders;
+    private final Map<String, String> httpBody;
 
-    String getParameterByKey(final String key);
+    private final Map<String, String> cookies;
 
-    Optional<String> getCookie(String key);
+    public HttpRequest(HttpRequestStartLine httpRequestStartLine, HttpHeader httpHeaders, Map<String, String> httpBody) {
+        this.httpRequestStartLine = httpRequestStartLine;
+        this.httpHeaders = httpHeaders;
+        this.httpBody = httpBody;
+        this.cookies = HttpRequestUtils.parseCookies(httpHeaders.get("Cookie"));
+    }
+
+    public HttpHeader getHttpHeader() {
+        return this.httpHeaders;
+    }
+
+    public String getParameterByKey(String key) {
+        return this.httpBody.getOrDefault(key, null);
+    }
+
+    public Optional<String> getCookie(String key) {
+        return Optional.ofNullable(cookies.getOrDefault(key, null));
+    }
+
+    public String getRequestPath() {
+        return this.httpRequestStartLine.getUrl();
+    }
 }
