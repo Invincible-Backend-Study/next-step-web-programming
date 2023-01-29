0d9f290162dab7876f2ca0f9c8dabd4395a4046d
HiiWee <wpdlzhf159@gmail.com>
2023-01-30 07:14:24 +0900

    refactor: 사용하지 않는 코드 및 메소드 삭제

--- web-application-server-gradle/src/main/java/webserver/RequestHandler.java
+++ web-application-server-gradle/src/main/java/webserver/RequestHandler.java
@@ -26,36 +26,11 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream();
              OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
 
             HttpRequest httpRequest = new HttpRequest(in);
             HttpResponse httpResponse = new HttpResponse(out);
             FrontController frontController = new FrontController(httpRequest, httpResponse);
             frontController.doProcess();
-//            byte[] fileBytes = Files.readAllBytes(new File("./webapp" + requestLine.getUri()).toPath());
-//            DataOutputStream dos = new DataOutputStream(out);
-//            response200Header(dos, fileBytes.length);
-//            responseBody(dos, fileBytes);
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
         } catch (IOException e) {
             log.error(e.getMessage());
         }
