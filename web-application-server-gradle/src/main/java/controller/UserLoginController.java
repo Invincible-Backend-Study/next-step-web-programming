c8dfffece8cdaa0136455fb37949d92814228ae2
wnsgh12s <junho1273@gmail.com>
2023-01-30 19:29:40 +0900

    sytle: 로그인 성공 여부 매서드 분리

--- web-application-server-gradle/src/main/java/controller/UserLoginController.java
+++ web-application-server-gradle/src/main/java/controller/UserLoginController.java
@@ -14,6 +14,10 @@ public class UserLoginController implements Controller {
     public void use(final HttpRequest httpRequest, final HttpResponse httpResponse) throws IOException {
         Map<String, String> queryString = HttpRequestUtils.parseQueryString(httpRequest.parseHttpRequestBody());
         User user = DataBase.findUserById(queryString.get("userId"));
+        judgementLogin(httpResponse, queryString, user);
+    }
+
+    private static void judgementLogin(HttpResponse httpResponse, Map<String, String> queryString, User user) {
         if (user != null && user.getPassword().equals(queryString.get("password"))) {
             httpResponse.loginSucess();
             return;
