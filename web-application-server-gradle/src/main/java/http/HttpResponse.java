c1191ea5139ea4c555f12b1b1fc7e4539c653570
wnsgh12s <junho1273@gmail.com>
2023-01-30 19:28:10 +0900

    feat: Accept Header를 가져와 자동으로 content-Type 할당

--- web-application-server-gradle/src/main/java/http/HttpResponse.java
+++ web-application-server-gradle/src/main/java/http/HttpResponse.java
@@ -1,16 +1,14 @@
 package http;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import webserver.RequestHandler;
-
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
 
 public class HttpResponse {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -21,12 +19,9 @@ public class HttpResponse {
     }
 
     private void response200Header(final DataOutputStream dos, int lengthOfBodyContent, String accept) {
-        if (accept.contains("css")) {
-            response200HeaderWithCss(dos, lengthOfBodyContent);
-        }
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: */* \r\n");
+            dos.writeBytes("Content-Type:" + accept.split(",")[0].trim() + "\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
