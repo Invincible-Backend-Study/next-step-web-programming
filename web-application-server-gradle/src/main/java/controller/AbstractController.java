6ee06dace340aba21fe1f91930bb8fa42493eade
HiiWee <wpdlzhf159@gmail.com>
2023-01-31 18:40:15 +0900

    refactor: 사용하지 않는 http method 메소드 실행시 예외 발생하도록 변경

--- web-application-server-gradle/src/main/java/controller/AbstractController.java
+++ web-application-server-gradle/src/main/java/controller/AbstractController.java
@@ -18,15 +18,12 @@ public abstract class AbstractController implements Controller {
         }
     }
 
-    /**
-     * abstract는 좋지 않은것 같음..!
-     * 사용자가 원하는 메소드만 오버라이딩 할 수 있도록하고 default는 4xx오류페이지를 보여주는게 알맞은 방향같다..!
-     */
     protected void doGet(final HttpRequest request, final HttpResponse response) throws IOException {
-        // 404
+        throw new RuntimeException("[ERROR] Undefined URI about GET Method");
     }
 
     protected void doPost(final HttpRequest request, final HttpResponse response) throws IOException {
+        throw new RuntimeException("[ERROR] Undefined URI about POST Method");
 
     }
 }
