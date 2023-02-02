ecb86a78393f52e942b4f1438f5d1b69d0b4cd51
Basak <chgy2131@naver.com>
2023-02-02 16:27:50 +0900

    fix: @WebServlet mapping을 위해 classes path 정보 추가

--- jwp-basic-gradle/src/test/java/next/WebServerLauncher.java
+++ jwp-basic-gradle/src/test/java/next/WebServerLauncher.java
@@ -2,7 +2,11 @@ package next;
 
 import java.io.File;
 
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,12 +14,21 @@ public class WebServerLauncher {
     private static final Logger logger = LoggerFactory.getLogger(WebServerLauncher.class);
 
     public static void main(String[] args) throws Exception {
+        String servletClassPath = "build/classes";
         String webappDirLocation = "webapp/";
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8080);
 
-        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        StandardContext context = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
         logger.info("configuring app with basedir: {}", new File("./" + webappDirLocation).getAbsolutePath());
+
+        //-----------------------------------------------------------------------------------------
+        File additionWebInfClasses = new File(servletClassPath);
+        WebResourceRoot resources = new StandardRoot(context);
+        resources.addPreResources(
+                new DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClasses.getAbsolutePath(), "/"));
+        context.setResources(resources);
+        //-----------------------------------------------------------------------------------------
 
         tomcat.start();
         tomcat.getServer().await();
