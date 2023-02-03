ae64c9c15fdfc226f3365e0ec4cee55a2a5afefb
Basak <chgy2131@naver.com>
2023-02-03 17:28:39 +0900

    feat: 세션 관련 클래스 뼈대 작성

---
+++ web-application-server-gradle/src/main/java/webserver/http/MyHttpSession.java
@@ -0,0 +1,23 @@
+package webserver.http;
+
+public class MyHttpSession {
+    public String getId() {
+        return "";
+    }
+
+    public void setAttribute(String name, Object value) {
+
+    }
+
+    public Object getAttribute(String name) {
+        return "";
+    }
+
+    public void removeAttribute(String name) {
+
+    }
+
+    public void invalidate() {
+
+    }
+}
