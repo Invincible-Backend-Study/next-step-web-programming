48d0356fbe7f1410d27e25fbd213961bab7449ba
HiiWee <wpdlzhf159@gmail.com>
2023-02-04 23:07:12 +0900

    feat: 초기 session id 정의 기능 추가

--- web-application-server-gradle/src/main/java/customwebserver/FrontController.java
+++ web-application-server-gradle/src/main/java/customwebserver/FrontController.java
@@ -9,6 +9,7 @@ import customwebserver.http.HttpResponse;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,8 +31,19 @@ public class FrontController {
         this.httpResponse = httpResponse;
     }
 
+
     public void doProcess() throws IOException {
+        sendRequestDataToResponse();
         requestDispatch();
+    }
+
+    /**
+     * 응답에 필요한 데이터를 httpRequest에서 전달
+     */
+    private void sendRequestDataToResponse() {
+        if (httpRequest.getCookies().getCookie("MYJESSIONID") == null) {
+            httpResponse.addCookie("MYJESSIONID", UUID.randomUUID().toString());
+        }
     }
 
     /**
