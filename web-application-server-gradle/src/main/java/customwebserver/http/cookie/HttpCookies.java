331da7d811782c228b79ff90eb9d71efa638cd93
HiiWee <wpdlzhf159@gmail.com>
2023-02-04 23:08:58 +0900

    feat: 쿠키를 가져오기 위한 메소드 추가

--- web-application-server-gradle/src/main/java/customwebserver/http/cookie/HttpCookies.java
+++ web-application-server-gradle/src/main/java/customwebserver/http/cookie/HttpCookies.java
@@ -2,11 +2,18 @@ package customwebserver.http.cookie;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HttpCookies {
-    private final Map<String, Object> httpCookies = new HashMap<>();
+    private static final Logger log = LoggerFactory.getLogger(HttpCookies.class);
+    private final Map<String, String> httpCookies = new HashMap<>();
 
     public HttpCookies() {
+    }
+
+    public HttpCookies(final Map<String, String> httpCookies) {
+        this.httpCookies.putAll(httpCookies);
     }
 
     public String createCookieMessage() {
@@ -25,7 +32,11 @@ public class HttpCookies {
         return "";
     }
 
-    public void addCookie(final String cookieKey, final Object value) {
+    public void addCookie(final String cookieKey, final String value) {
         httpCookies.put(cookieKey, value);
+    }
+
+    public String getCookie(final String cookieKey) {
+        return httpCookies.get(cookieKey);
     }
 }
