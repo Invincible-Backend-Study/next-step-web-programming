050a5284f6203c00cb32d38d43e64b35a3a535a4
HiiWee <wpdlzhf159@gmail.com>
2023-02-04 23:10:41 +0900

    feat: 비밀번호 검증 로직 추가

--- web-application-server-gradle/src/main/java/model/User.java
+++ web-application-server-gradle/src/main/java/model/User.java
@@ -29,8 +29,13 @@ public class User {
         return email;
     }
 
+    public boolean containPassword(final String password) {
+        return this.password.equals(password);
+    }
+
     @Override
     public String toString() {
         return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
     }
+
 }
