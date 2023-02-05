3a86d9ce438d60c4e839f52876088b695144eb2b
HiiWee <wpdlzhf159@gmail.com>
2023-02-06 01:11:02 +0900

    fix: 컨트롤러 사용하지 않는 http method 구현 메서드 삭제

--- jwp-basic-gradle/src/main/java/core/web/controller/AbstractController.java
+++ jwp-basic-gradle/src/main/java/core/web/controller/AbstractController.java
@@ -1,12 +1,15 @@
 package core.web.controller;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AbstractController implements Controller {
 
     @Override
-    public String execute(final HttpServletRequest request, final HttpServletResponse response) {
+    public String execute(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException {
         HttpMethod method = HttpMethod.valueOf(request.getMethod());
         if (method.equals(HttpMethod.GET)) {
             return doGet(request, response);
@@ -17,10 +20,13 @@ public class AbstractController implements Controller {
         throw new IllegalArgumentException("[ERROR] 요청과 일치되는 메소드가 없습니다.");
     }
 
-    protected String doGet(final HttpServletRequest request, final HttpServletResponse response) {
+    // HTTP Method: GET
+    protected String doGet(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException {
         throw new IllegalArgumentException("[ERROR] 정의되지 않은 요청입니다.");
     }
 
+    // HTTP Method: POST
     protected String doPost(final HttpServletRequest request, final HttpServletResponse response) {
         throw new IllegalArgumentException("[ERROR] 정의되지 않은 요청입니다.");
     }
--- jwp-basic-gradle/src/main/java/core/web/controller/Controller.java
+++ jwp-basic-gradle/src/main/java/core/web/controller/Controller.java
@@ -1,8 +1,10 @@
 package core.web.controller;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Controller {
-    String execute(HttpServletRequest request, HttpServletResponse response);
+    String execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
 }
