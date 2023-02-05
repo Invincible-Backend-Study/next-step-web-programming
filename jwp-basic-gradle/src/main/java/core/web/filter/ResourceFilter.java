504944e78ce5c95514111c63084e9ae89716cc4f
HiiWee <wpdlzhf159@gmail.com>
2023-02-06 03:19:12 +0900

    refactor: 정적 리소스 필터 간소화

--- jwp-basic-gradle/src/main/java/core/web/filter/ResourceFilter.java
+++ jwp-basic-gradle/src/main/java/core/web/filter/ResourceFilter.java
@@ -4,28 +4,22 @@ import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @WebFilter(urlPatterns = {"/css/*", "/js/*", "/fonts/*", "/images/*", "/favicon.ico"})
 public class ResourceFilter implements Filter {
-    private static final Logger log = LoggerFactory.getLogger(ResourceFilter.class);
-    private RequestDispatcher defaultRequestDispatcher;
 
     @Override
     public void init(final FilterConfig filterConfig) throws ServletException {
-        defaultRequestDispatcher = filterConfig.getServletContext().getNamedDispatcher("default");
     }
 
     @Override
     public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
             throws IOException, ServletException {
-        defaultRequestDispatcher.forward(request, response);
+        request.getServletContext().getNamedDispatcher("default").forward(request, response);
     }
 
     @Override
