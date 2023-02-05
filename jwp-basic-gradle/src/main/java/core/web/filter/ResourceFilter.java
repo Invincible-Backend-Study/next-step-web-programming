0a23213ad4eaa1f68d595e8505c40d6aeac182f5
HiiWee <wpdlzhf159@gmail.com>
2023-02-06 01:12:19 +0900

    refactor: 정적 리소스를 관리하는 DefaultServlet을 이용해 정적 리소스 필터링 하도록 변경

--- jwp-basic-gradle/src/main/java/core/web/filter/ResourceFilter.java
+++ jwp-basic-gradle/src/main/java/core/web/filter/ResourceFilter.java
@@ -4,28 +4,28 @@ import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @WebFilter(urlPatterns = {"/css/*", "/js/*", "/fonts/*", "/images/*", "/favicon.ico"})
 public class ResourceFilter implements Filter {
     private static final Logger log = LoggerFactory.getLogger(ResourceFilter.class);
+    private RequestDispatcher defaultRequestDispatcher;
 
     @Override
     public void init(final FilterConfig filterConfig) throws ServletException {
-
+        defaultRequestDispatcher = filterConfig.getServletContext().getNamedDispatcher("default");
     }
 
     @Override
     public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
             throws IOException, ServletException {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-        request.getRequestDispatcher(httpServletRequest.getRequestURI()).forward(request, response);
+        defaultRequestDispatcher.forward(request, response);
     }
 
     @Override
