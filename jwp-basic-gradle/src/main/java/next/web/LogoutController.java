52d62e03a58124f0a2ac9e1f2f2f59ed2fdcc4bb
wnsgh12s <junho1273@gmail.com>
2023-02-06 10:02:33 +0900

    feat: 로그아웃 기능

---
+++ jwp-basic-gradle/src/main/java/next/web/LogoutController.java
@@ -0,0 +1,17 @@
+package next.web;
+
+import next.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LogoutController implements Controller {
+
+    @Override
+    public String execute(HttpServletRequest req, HttpServletResponse res) {
+        HttpSession session = req.getSession();
+        session.removeAttribute("user");
+        return "redirect:/";
+    }
+}
