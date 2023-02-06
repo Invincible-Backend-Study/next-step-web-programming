74f462fa25c4cf9ab69cbe650aa29d84119baeb8
wnsgh12s <junho1273@gmail.com>
2023-02-06 10:03:26 +0900

    feat: 회원가입 기능

---
+++ jwp-basic-gradle/src/main/java/next/web/CreateUserController.java
@@ -0,0 +1,36 @@
+package next.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import next.model.User;
+
+import next.mvc.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import core.db.DataBase;
+
+public class CreateUserController implements Controller {
+    private static final long serialVersionUID = 1L;
+    private static final Logger log = LoggerFactory.getLogger(CreateUserController.class);
+
+    @Override
+    public String execute(HttpServletRequest req, HttpServletResponse res) {
+        User user = new User(
+                req.getParameter("userId"),
+                req.getParameter("password"),
+                req.getParameter("name"),
+                req.getParameter("email")
+        );
+        log.debug("user : {}", user.getUserId());
+        DataBase.addUser(user);
+        return "/user/list.jsp";
+    }
+}
