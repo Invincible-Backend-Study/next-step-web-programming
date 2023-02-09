bc6d4b708484df600e9229157624e614c08daf6e
wnsgh12s <junho1273@gmail.com>
2023-02-09 14:30:05 +0900

    feat:  빌드 파일 위치 변경

--- jwp-basic-gradle/src/main/java/next/WebServerLauncher.java
+++ jwp-basic-gradle/src/main/java/next/WebServerLauncher.java
@@ -1,18 +1,27 @@
 package next;
 
+import java.net.URI;
 import org.apache.catalina.LifecycleException;
+import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
 
 import javax.servlet.ServletException;
 import java.io.File;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
 
 public class WebServerLauncher {
     public static void main(String[] args) throws ServletException, LifecycleException {
-        String webappPath = "./webapp";
+        String webappPath = "webapp/";
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8080);
-
-        tomcat.addWebapp("/",new File(webappPath).getAbsolutePath());
+        StandardContext context = (StandardContext) tomcat.addWebapp("/",
+                new File(webappPath).getAbsolutePath());
+        File additionWebInfClasses = new File("out/production/classes");
+        StandardRoot resources = new StandardRoot(context);
+        resources.addPreResources(
+                new DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClasses.getAbsolutePath(), "/"));
+        context.setResources(resources);
         tomcat.start();
         tomcat.getServer().await();
     }
