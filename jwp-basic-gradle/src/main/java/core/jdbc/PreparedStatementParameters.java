054aafb7658b07b14c13ba6e58ec265a4c41cfb4
Basak <chgy2131@naver.com>
2023-02-10 17:43:43 +0900

    refactor(ResultSetMapper): FunctionalInterface 지정을 통한 람다식 적용

--- jwp-basic-gradle/src/main/java/core/jdbc/PreparedStatementParameters.java
+++ jwp-basic-gradle/src/main/java/core/jdbc/PreparedStatementParameters.java
@@ -3,6 +3,7 @@ package core.jdbc;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface PreparedStatementParameters {
     void setParameters(PreparedStatement pstmt) throws SQLException;
 }
