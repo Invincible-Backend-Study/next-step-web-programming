e50e7996743ea264d843254c56c21071a3b96e4f
HiiWee <wpdlzhf159@gmail.com>
2023-02-11 21:57:07 +0900

    feat: 함수형 인터페이스 적용

--- jwp-basic-gradle/src/main/java/core/jdbc/RowMapper.java
+++ jwp-basic-gradle/src/main/java/core/jdbc/RowMapper.java
@@ -3,6 +3,7 @@ package core.jdbc;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface RowMapper {
     Object mapRow(ResultSet resultSet) throws SQLException;
 }
