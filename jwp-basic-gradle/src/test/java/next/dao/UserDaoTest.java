09658cec086915f3b7bc078808e13f1e92b616db
wnsgh12s <junho1273@gmail.com>
2023-02-11 14:49:36 +0900

    test: find_all_users

--- jwp-basic-gradle/src/test/java/next/dao/UserDaoTest.java
+++ jwp-basic-gradle/src/test/java/next/dao/UserDaoTest.java
@@ -3,7 +3,12 @@ package next.dao;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import next.model.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class UserDaoTest {
@@ -19,12 +24,24 @@ public class UserDaoTest {
 
     @Test
     public void update_test() throws Exception {
-        User user = new User("qwe", "123", "이준호", "javajigi@email.com");
+        User user = new User("test", "123", "이준호", "javajigi@email.com");
         User expected = new User("qwe", "2", "123", "test@email.com");
         UserDao userDao = new UserDao();
+        userDao.insert(user);
         userDao.update(expected, user.getUserId());
         User actual = userDao.findByUserId(expected.getUserId());
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void find_all_users() throws Exception {
+        UserDao userDao = new UserDao();
+        User user = new User("wnsgh12s", "123", "이준호", "junho1273@gmail.com");
+        User admin = new User("admin", "password", "자바지기", "admin@slipp.net");
+        userDao.insert(user);
+        List<User> findAlluser = userDao.findAll();
+        List<User> expected = Arrays.asList(user, admin);
+        assertEquals(findAlluser.containsAll(expected), true);
     }
 
 }
