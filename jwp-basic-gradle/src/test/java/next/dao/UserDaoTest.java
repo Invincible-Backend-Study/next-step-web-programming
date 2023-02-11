a52e18b4971fb74172b2d6ea48eb532b2dabd45f
HiiWee <wpdlzhf159@gmail.com>
2023-02-11 18:19:52 +0900

    test: findAll 메소드에 대한 테스트 추가해 템플릿 검증

--- jwp-basic-gradle/src/test/java/next/dao/UserDaoTest.java
+++ jwp-basic-gradle/src/test/java/next/dao/UserDaoTest.java
@@ -1,20 +1,39 @@
 package next.dao;
 
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
 import next.model.User;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class UserDaoTest {
 
     @Test
-    public void crud() throws Exception {
+    public void crud() {
         User expected = new User("userId", "password", "name", "javajigi@email.com");
         UserDao userDao = new UserDao();
         userDao.insert(expected);
 
         User actual = userDao.findByUserId(expected.getUserId());
-        Assertions.assertThat(actual).isEqualTo(expected);
+        assertThat(actual).isEqualTo(expected);
     }
 
+    @Test
+    void findAll() {
+        // given
+        User expected = new User("userId", "password", "name", "test@email.com");
+        UserDao userDao = new UserDao();
+
+        // when
+        userDao.insert(expected);
+        List<User> users = userDao.findAll();
+
+        // then
+        Assertions.assertAll(() -> {
+            assertThat(users.size()).isEqualTo(2);
+            assertThat(users).contains(expected);
+        });
+    }
 }
