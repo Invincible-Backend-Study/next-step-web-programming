f8ec4aaa79ccb64e4a73b52663f130831e32149b
wnsgh12s <junho1273@gmail.com>
2023-02-11 13:17:13 +0900

    feat: 모든 유저의 목록을 반환 하는 기능

--- jwp-basic-gradle/src/main/java/next/dao/UserDao.java
+++ jwp-basic-gradle/src/main/java/next/dao/UserDao.java
@@ -1,11 +1,16 @@
 package next.dao;
 
+import java.sql.Array;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import core.jdbc.ConnectionManager;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import next.model.User;
 
@@ -90,28 +95,32 @@ public class UserDao {
         }
     }
 
-    public Collection<User> findAll(User user) throws SQLException {
+    public List<User> findAll() throws SQLException {
         Connection con = null;
-        PreparedStatement pstmt = null;
+        Statement pstmt = null;
+        ResultSet rs = null;
         try {
             con = ConnectionManager.getConnection();
-            System.out.println(user.getUserId());
-            String sql = "SELECT userId, password, name, email FROM USERS";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, user.getUserId());
-            pstmt.setString(2, user.getPassword());
-            pstmt.setString(3, user.getName());
-            pstmt.setString(4, user.getEmail());
-            pstmt.executeUpdate();
+            String sql = "SELECT userId, name, password,email FROM USERS";
+            pstmt = con.createStatement();
+            rs = pstmt.executeQuery(sql);
+            List<User> userList = new ArrayList<>();
+            while (rs.next()) {
+                User user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
+                        rs.getString("email"));
+                userList.add(user);
+            }
+            return userList;
         } finally {
+            if (rs != null) {
+                rs.close();
+            }
             if (pstmt != null) {
                 pstmt.close();
             }
-
             if (con != null) {
                 con.close();
             }
         }
-        return null;
     }
 }
