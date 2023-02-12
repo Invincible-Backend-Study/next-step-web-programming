cac88c3f84c811834854db55b8bfd56359e26d5e
wnsgh12s <54989191+wnsgh12s@users.noreply.github.com>
2023-02-12 15:42:59 +0900

    refactor : try-with-resourece 문법 사용

--- jwp-basic-gradle/src/main/java/core/jdbc/JdbcTemplete.java
+++ jwp-basic-gradle/src/main/java/core/jdbc/JdbcTemplete.java
@@ -8,23 +8,12 @@ import java.sql.SQLException;
 
 public abstract class JdbcTemplete {
     public void insert(String sql) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
+        try (Connection con = ConnectionManager.getConnection(); PreparedStatement pstmt = con.prepareStatement(sql)) {
             setParameters(pstmt);
             pstmt.executeUpdate();
-        } finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-
-            if (con != null) {
-                con.close();
-            }
         }
     }
+
 
     abstract public void setParameters(PreparedStatement pstsmt) throws SQLException;
 }
