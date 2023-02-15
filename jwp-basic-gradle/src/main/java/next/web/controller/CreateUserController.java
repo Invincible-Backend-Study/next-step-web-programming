95e8cb81ac8fcb6a5a2122d5bde3c4b98755ba4a
HiiWee <wpdlzhf159@gmail.com>
2023-02-15 16:15:22 +0900

    refactor: 사용자 회원가입 컨트롤러 Controller 인터페이스 구현으로 변경

--- jwp-basic-gradle/src/main/java/next/web/controller/CreateUserController.java
+++ jwp-basic-gradle/src/main/java/next/web/controller/CreateUserController.java
@@ -1,6 +1,7 @@
 package next.web.controller;
 
 import core.mvcframework.AbstractController;
+import core.mvcframework.Controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import next.model.User;
@@ -8,18 +9,13 @@ import next.web.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class CreateUserController extends AbstractController {
+public class CreateUserController implements Controller {
     private static final Logger log = LoggerFactory.getLogger(CreateUserController.class);
 
     private final UserService userService = new UserService();
 
     @Override
-    protected String doGet(final HttpServletRequest request, final HttpServletResponse response) {
-        return "user/form";
-    }
-
-    @Override
-    protected String doPost(final HttpServletRequest request, final HttpServletResponse response) {
+    public String execute(final HttpServletRequest request, final HttpServletResponse response) {
         User user = new User(
                 request.getParameter("userId"),
                 request.getParameter("password"),
