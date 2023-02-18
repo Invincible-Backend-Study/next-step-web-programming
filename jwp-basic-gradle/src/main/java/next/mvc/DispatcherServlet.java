9c92baf71657e2a010f9a25e2eec9d969ceb26c7
wnsgh12s <54989191+wnsgh12s@users.noreply.github.com>
2023-02-18 20:00:42 +0900

    feat: dispatcherServlet  파라미터  loadOnStartup = 1

--- jwp-basic-gradle/src/main/java/next/mvc/DispatcherServlet.java
+++ jwp-basic-gradle/src/main/java/next/mvc/DispatcherServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "dispatcher", urlPatterns = "/")
+@WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
 public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final String REDIRECT_PREFIX = "redirect:";
