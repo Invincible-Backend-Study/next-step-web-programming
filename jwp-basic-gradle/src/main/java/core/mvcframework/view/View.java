aecda07656dadc4e2ece1c13685277a6d3c8180a
HiiWee <wpdlzhf159@gmail.com>
2023-02-19 23:16:40 +0900

    feat: View 인터페이스 추가

---
+++ jwp-basic-gradle/src/main/java/core/mvcframework/view/View.java
@@ -0,0 +1,9 @@
+package core.mvcframework.view;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface View {
+    
+    void render(HttpServletRequest request, HttpServletResponse response) throws Exception;
+}
