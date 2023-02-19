712efba9379047b5ad5a9d6c687a40ee4fec49dd
wnsgh12s <54989191+wnsgh12s@users.noreply.github.com>
2023-02-20 03:12:06 +0900

    feat: : answerId가 자동 세팅 되면 값을 저장할 Keyholder 생성

---
+++ jwp-basic-gradle/src/main/java/core/jdbc/KeyHolder.java
@@ -0,0 +1,13 @@
+package core.jdbc;
+
+public class KeyHolder {
+    private int id;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
