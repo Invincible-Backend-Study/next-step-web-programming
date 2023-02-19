dfbe171b4f1c2a8ccd98165a777ab0d8e1b47fa4
HiiWee <wpdlzhf159@gmail.com>
2023-02-20 02:26:53 +0900

    feat: ModelAndView 생성을 도와주는 컨트롤러 추가

---
+++ jwp-basic-gradle/src/main/java/core/mvcframework/controller/AbstractController.java
@@ -0,0 +1,15 @@
+package core.mvcframework.controller;
+
+import core.mvcframework.ModelAndView;
+import core.mvcframework.view.JsonView;
+import core.mvcframework.view.JspView;
+
+public abstract class AbstractController implements Controller {
+    protected ModelAndView jspView(String forwardUri) {
+        return new ModelAndView(new JspView(forwardUri));
+    }
+
+    protected ModelAndView jsonView() {
+        return new ModelAndView(new JsonView());
+    }
+}
