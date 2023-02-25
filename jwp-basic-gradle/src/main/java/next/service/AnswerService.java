1a9d37846171faf79b090810217d78e8506ae970
HiiWee <wpdlzhf159@gmail.com>
2023-02-25 21:24:25 +0900

    remove: 사용하지 않는 컨트롤러 및 관련 메소드 삭제

--- jwp-basic-gradle/src/main/java/next/service/AnswerService.java
+++ jwp-basic-gradle/src/main/java/next/service/AnswerService.java
@@ -6,28 +6,21 @@ import next.model.Answer;
 import next.model.Question;
 
 public class AnswerService {
+
     private final AnswerDao answerDao = new AnswerDao();
     private final QuestionDao questionDao = new QuestionDao();
 
-    public void insertNewAnswer(final Answer answer) {
-        answerDao.insert(answer);
-        increaseCountOfAnswer(answer.getQuestionId());
-    }
-
-    private void increaseCountOfAnswer(final Long questionId) {
-        Question findQuestion = questionDao.findByQuestionId(questionId);
-        questionDao.updateCountOfAnswerById(questionId, findQuestion.getCountOfAnswer() + 1);
-    }
-
-    /**
-     * AJAX 실습 insert
-     */
     public Answer insertAnswer(final Answer answer) {
         if (answerDao.insert(answer) >= 1) {
             increaseCountOfAnswer(answer.getQuestionId());
             return answer;
         }
         return null;
+    }
+
+    private void increaseCountOfAnswer(final Long questionId) {
+        Question findQuestion = questionDao.findByQuestionId(questionId);
+        questionDao.updateCountOfAnswerById(questionId, findQuestion.getCountOfAnswer() + 1);
     }
 
     public int deleteAnswer(final long answerId) {
