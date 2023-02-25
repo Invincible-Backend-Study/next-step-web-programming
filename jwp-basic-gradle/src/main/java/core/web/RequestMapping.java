2204b63e4d4c4b0f948ff3fdbfe28d420fff7957
Basak <chgy2131@naver.com>
2023-02-25 23:10:48 +0900

    feat(question): 질문 목록을 JSON 데이터로 조회할수 있도록 구현

--- jwp-basic-gradle/src/main/java/core/web/RequestMapping.java
+++ jwp-basic-gradle/src/main/java/core/web/RequestMapping.java
@@ -1,13 +1,9 @@
 package core.web;
 
+import next.api.qna.*;
 import next.common.controller.HomeController;
-import next.controller.*;
 import next.api.auth.LoginController;
 import next.api.auth.LogoutController;
-import next.api.qna.AnswerController;
-import next.api.qna.ListQuestionController;
-import next.api.qna.QuestionController;
-import next.api.qna.QuestionFormController;
 import next.api.user.CreateUserController;
 import next.api.user.ListUserController;
 import next.api.user.UpdateUserController;
@@ -19,16 +15,24 @@ import java.util.Map;
 public class RequestMapping {
     private static final Map<String, Controller> controllers = new HashMap<>();
     static {
-        controllers.put("/user/create", new CreateUserController());
+        // auth
         controllers.put("/user/login", new LoginController());
         controllers.put("/user/logout", new LogoutController());
+
+        // user
+        controllers.put("/user/create", new CreateUserController());
         controllers.put("/user/list", new ListUserController());
         controllers.put("/user/form", new UpdateUserFormController());
         controllers.put("/user/update", new UpdateUserController());
+
+        // qna
+        controllers.put("/api/qna/list", new ListQuestionApiController());
         controllers.put("/question/list", new ListQuestionController());
         controllers.put("/question", new QuestionController());
         controllers.put("/question/form", new QuestionFormController());
         controllers.put("/answer", new AnswerController());
+
+        // home
         controllers.put("/index", new HomeController());
     }
 
---
+++ jwp-basic-gradle/src/main/java/next/api/qna/ListQuestionApiController.java
@@ -0,0 +1,33 @@
+package next.api.qna;
+
+import core.web.ModelAndView;
+import next.api.qna.dao.QuestionDao;
+import next.api.qna.model.Question;
+import next.common.controller.AbstractController;
+import next.common.view.JsonView;
+import next.common.view.JspView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ListQuestionApiController extends AbstractController {
+    private static final Logger log = LoggerFactory.getLogger(ListQuestionApiController.class);
+    private final QuestionDao questionDao = new QuestionDao();
+
+    @Override
+    protected ModelAndView doGet(HttpServletRequest request, HttpServletResponse response) {
+        List<Question> questions = null;
+        try {
+            questions = questionDao.findAll();
+            request.setAttribute("questions", questions);
+        } catch (SQLException e) {
+            log.error(e.toString());
+        }
+
+        return new ModelAndView(new JsonView()).addModel("questions", questions);
+    }
+}
