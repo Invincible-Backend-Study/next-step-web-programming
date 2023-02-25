270778845262e5cdba7e763e55a63f2b16d820f4
HiiWee <wpdlzhf159@gmail.com>
2023-02-25 21:05:02 +0900

    feat: 로그인 필터 뼈대 코드 추가

---
+++ jwp-basic-gradle/src/main/java/core/web/filter/LoginCheckFilter.java
@@ -0,0 +1,37 @@
+package core.web.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebFilter
+public class LoginCheckFilter implements Filter {
+
+    private static final Logger log = LoggerFactory.getLogger(LoginCheckFilter.class);
+
+    @Override
+    public void init(final FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        log.debug("LoginCheckFilter URI={}", httpServletRequest.getRequestURI());
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
