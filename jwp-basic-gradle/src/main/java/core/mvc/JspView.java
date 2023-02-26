2229b116d4663e796f077dd59f6df0dee433c366
JaehongDev <println@kakao.com>
2023-02-26 22:19:25 +0900

    feature: view vo기능 추가

--- jwp-basic-gradle/src/main/java/core/mvc/JspView.java
+++ jwp-basic-gradle/src/main/java/core/mvc/JspView.java
@@ -2,10 +2,13 @@ package core.mvc;
 
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class JspView implements View {
     private static final String DEFAULT_REDIRECT_PREFIX = "redirect: ";
     private final String viewName;
@@ -25,9 +28,27 @@ public class JspView implements View {
             for (String key : keys) {
                 request.setAttribute(key, model.get(key));
             }
+            log.info("key {}", keys);
         }
 
         final var requestDispatcher = request.getRequestDispatcher(viewName);
         requestDispatcher.forward(request, response);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        JspView jspView = (JspView) o;
+        return Objects.equals(viewName, jspView.viewName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(viewName);
     }
 }
