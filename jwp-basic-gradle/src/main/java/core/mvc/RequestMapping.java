842a1ea93a2bd8aadd3c9f22cb2c8aa81077d362
JaehongDev <println@kakao.com>
2023-02-26 22:23:33 +0900

    feature: 매핑 기능 추가

--- jwp-basic-gradle/src/main/java/core/mvc/RequestMapping.java
+++ jwp-basic-gradle/src/main/java/core/mvc/RequestMapping.java
@@ -2,14 +2,20 @@ package core.mvc;
 
 import java.util.HashMap;
 import java.util.Map;
-import next.controller.answer.AddAnswerController;
-import next.controller.answer.DeleteAnswerController;
-import next.controller.answer.GetQuestionController;
-import next.controller.answer.ShowController;
-import next.controller.question.CreateQnAFormController;
-import next.controller.question.CreateQnaActionController;
+import next.answer.controller.AddAnswerController;
+import next.answer.controller.DeleteAnswerController;
+import next.answer.controller.GetQuestionController;
+import next.answer.controller.ShowController;
+import next.answer.dao.AnswerDao;
+import next.qna.dao.QuestionDao;
+import next.qna.service.DeleteQnaService;
+import next.qna.ui.CreateQnAFormController;
+import next.qna.ui.CreateQnaActionController;
+import next.qna.ui.DeleteQnaController;
+import next.qna.ui.HomeController;
+import next.qna.ui.UpdateQnaActionController;
+import next.qna.ui.UpdateQnaFormController;
 import next.user.controller.CreateUserController;
-import next.user.controller.HomeController;
 import next.user.controller.ListUserController;
 import next.user.controller.UpdateFormController;
 import next.user.controller.UpdateUserController;
@@ -21,9 +27,7 @@ import org.slf4j.LoggerFactory;
 public class RequestMapping {
     private static final Logger log = LoggerFactory.getLogger(RequestMapping.class);
     private static final String DEFAULT_VIEW_PATH = "/WEB-INF";
-
     private final Map<String, AbstractController> mapping = new HashMap<>();
-
 
     public void initMapping() {
         mapping.put("/", new HomeController());
@@ -37,6 +41,9 @@ public class RequestMapping {
         mapping.put("/api/qna/show", new GetQuestionController());
         mapping.put("/qna/form", new CreateQnAFormController());
         mapping.put("/qna/create", new CreateQnaActionController());
+        mapping.put("/qna/update", new UpdateQnaActionController());
+        mapping.put("/qna/delete", new DeleteQnaController(new DeleteQnaService(new QuestionDao(), new AnswerDao())));
+        mapping.put("/qna/update/form", new UpdateQnaFormController());
         mapping.put("/api/qna/addAnswer", new AddAnswerController());
         mapping.put("/api/qna/deleteAnswer", new DeleteAnswerController());
 
