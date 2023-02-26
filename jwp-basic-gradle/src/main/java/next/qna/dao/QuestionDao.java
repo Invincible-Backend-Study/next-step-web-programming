15de7f36ad877955366d8e2b2f0b8454bbcf9d95
JaehongDev <println@kakao.com>
2023-02-26 22:21:55 +0900

    feature: 질문 삭제 기능 추가

--- jwp-basic-gradle/src/main/java/next/qna/dao/QuestionDao.java
+++ jwp-basic-gradle/src/main/java/next/qna/dao/QuestionDao.java
@@ -1,20 +1,35 @@
-package next.dao;
+package next.qna.dao;
 
-import static next.dao.sql.QuestionSql.FIND_BY_ID;
-import static next.dao.sql.QuestionSql.FindAll;
-import static next.dao.sql.QuestionSql.SAVE;
-import static next.dao.sql.QuestionSql.UPDATE_QUESTION_COUNT;
+import static next.qna.sql.QuestionSql.FIND_BY_ID;
+import static next.qna.sql.QuestionSql.FindAll;
+import static next.qna.sql.QuestionSql.SAVE;
+import static next.qna.sql.QuestionSql.UPDATE_QUESTION_COUNT;
 
 import core.jdbc.JdbcTemplate;
 import java.util.List;
-import next.model.Question;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import next.qna.domain.Question;
+import next.qna.sql.QuestionSql;
 
+@Slf4j
 public class QuestionDao {
-    private final JdbcTemplate jdbcTemplate = new JdbcTemplate();
+
+    private final JdbcTemplate jdbcTemplate = JdbcTemplate.getInstance();
+
+    public QuestionDao() {
+        log.info("question dao created");
+    }
+
+    public static QuestionDao getInstance() {
+        return QuestionDaoHolder.QUESTION_DAO;
+    }
+
     public List<Question> findAll() {
 
         return jdbcTemplate.query(FindAll,
-                preparedStatement -> {},
+                preparedStatement -> {
+                },
                 resultSet -> new Question(
                         resultSet.getLong("questionId"),
                         resultSet.getString("writer"),
@@ -26,7 +41,7 @@ public class QuestionDao {
 
     public Question findById(long questionId) {
         return jdbcTemplate.queryForObject(FIND_BY_ID,
-                preparedStatement -> preparedStatement.setLong(1,questionId),
+                preparedStatement -> preparedStatement.setLong(1, questionId),
                 resultSet -> new Question(
                         resultSet.getLong("questionId"),
                         resultSet.getString("writer"),
@@ -36,6 +51,10 @@ public class QuestionDao {
                         resultSet.getInt("countOfAnswer")
                 )
         );
+    }
+
+    public Optional<Question> findOptionalById(long questionId) {
+        return Optional.ofNullable(this.findById(questionId));
     }
 
     public void save(Question question) {
@@ -53,5 +72,24 @@ public class QuestionDao {
             preparedStatement.setLong(1, updateQuestion.getCountOfComment());
             preparedStatement.setLong(2, updateQuestion.getQuestionId());
         });
+    }
+
+    public void deleteById(long questionId) {
+        jdbcTemplate.update(QuestionSql.DELETE_BY_ID, preparedStatement -> {
+            preparedStatement.setLong(1, questionId);
+        });
+    }
+
+    public void update(Question question) {
+        jdbcTemplate.update(QuestionSql.UPDATE_QUESTION, preparedStatement -> {
+            preparedStatement.setString(1, question.getTitle());
+            preparedStatement.setString(2, question.getContents());
+            preparedStatement.setLong(3, question.getQuestionId());
+        });
+
+    }
+
+    private static class QuestionDaoHolder {
+        private static final QuestionDao QUESTION_DAO = new QuestionDao();
     }
 }
