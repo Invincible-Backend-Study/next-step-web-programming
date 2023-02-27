2391445f728467ef84c45523746523fa06415059
wnsgh12s <junho1273@gmail.com>
2023-02-27 11:44:50 +0900

    feat: QuestionService 생성

---
+++ jwp-basic-gradle/src/main/java/next/service/QuestionService.java
@@ -0,0 +1,39 @@
+package next.service;
+
+import java.util.List;
+import java.util.Objects;
+import next.dao.AnswerDao;
+import next.dao.QuestionDao;
+import next.model.Answer;
+import next.model.Question;
+import next.model.User;
+
+public class QuestionService {
+
+    private final QuestionDao questionDao = new QuestionDao();
+    private final AnswerDao answerDao = new AnswerDao();
+
+    public Question findByQuestionId(int questionId) {
+        return questionDao.findByQuestionId(questionId);
+    }
+
+    public boolean deleteQuestion(User user, Question question) {
+        List<Answer> answers = answerDao.findAllByQuestonId(question.getQuestionId());
+        boolean canDelete = answers.stream().allMatch(answer -> Objects.equals(answer.getWriter(), user.getName()));
+        if (canDelete) {
+            questionDao.deleteAnswer(question.getQuestionId());
+            answers.forEach(answer -> answerDao.deleteAnswer(answer.getAnswerId()));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public List<Question> findAll() {
+        return questionDao.findAll();
+    }
+
+    public void addQuestion(Question question) {
+        questionDao.addQuestion(question);
+    }
+}
