332fd244f342a149c446ae9bbe642bc1ce81d45f
Basak <chgy2131@naver.com>
2023-03-01 21:36:56 +0900

    refactor(auth): service 분리

--- jwp-basic-gradle/src/main/java/next/api/auth/controller/LoginController.java
+++ jwp-basic-gradle/src/main/java/next/api/auth/controller/LoginController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import next.api.auth.service.AuthService;
 import next.common.controller.AbstractController;
 import next.api.user.dao.UserDao;
 import next.api.user.model.User;
@@ -16,7 +17,7 @@ import java.sql.SQLException;
 
 public class LoginController extends AbstractController {
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
-    private final UserDao userDao = UserDao.getInstance();
+    private final AuthService authService = AuthService.getInstance();
 
     @Override
     protected ModelAndView doPost(HttpServletRequest request, HttpServletResponse response) {
@@ -31,19 +32,6 @@ public class LoginController extends AbstractController {
     }
 
     private User getLoginUser(String id, String password) {
-        User user = null;
-        try {
-            user = userDao.findByUserId(id);
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-        }
-
-        if (user == null) {
-            return null;
-        }
-        if (user.isAuthWith(id, password)) {
-            return user;
-        }
-        return null;
+        return authService.getUser(id, password);
     }
 }
---
+++ jwp-basic-gradle/src/main/java/next/api/auth/service/AuthService.java
@@ -0,0 +1,36 @@
+package next.api.auth.service;
+
+import next.api.auth.controller.LoginController;
+import next.api.user.dao.UserDao;
+import next.api.user.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.SQLException;
+
+public class AuthService {
+    private static final Logger log = LoggerFactory.getLogger(AuthService.class);
+    private static AuthService authService = new AuthService();
+    private AuthService() {};
+    public static AuthService getInstance() {
+        return authService;
+    }
+    private final UserDao userDao = UserDao.getInstance();
+
+    public User getUser(final String id, final String password) {
+        User user;
+        try {
+            user = userDao.findByUserId(id);
+            if (user == null) {
+                return null;
+            }
+            if (user.isAuthWith(id, password)) {
+                return user;
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+        }
+
+        return null;
+    }
+}
