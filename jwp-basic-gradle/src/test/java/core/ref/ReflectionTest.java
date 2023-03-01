6b8636e0bcec73768f5449704d77df482b83065b
JaehongDev <println@kakao.com>
2023-03-01 19:51:39 +0900

    test: Reflection을 활용한 메타데이터 출력

--- jwp-basic-gradle/src/test/java/core/ref/ReflectionTest.java
+++ jwp-basic-gradle/src/test/java/core/ref/ReflectionTest.java
@@ -1,11 +1,14 @@
 package core.ref;
 
-import org.junit.Test;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import next.qna.domain.Question;
+import next.user.entity.User;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import next.model.Question;
-import next.model.User;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -13,15 +16,50 @@ public class ReflectionTest {
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
-        logger.debug(clazz.getName());
+        logger.info(clazz.getName());
+        logger.info(clazz.getSimpleName());
+
+        logger.info("============ fields ===========");
+        for (var field : clazz.getDeclaredFields()) {
+            final var nameOfField = field.getName();
+            final var typeOfField = field.getType();
+            logger.info("{} {}", typeOfField, nameOfField);
+        }
+
+        logger.info("============ constructors =====");
+        for (var constructors : clazz.getConstructors()) {
+            final var modifierType = Modifier.toString(constructors.getModifiers()); // 생성자의 타입
+            final var parameterCountByConstructors = constructors.getParameterCount();
+            final var name = constructors.getName();
+
+            logger.info("{} {} 파라미터의 개수:{}", modifierType, name, parameterCountByConstructors);
+        }
+
+        logger.info("============ method ===========");
+
+        for (var methods : clazz.getDeclaredMethods()) {
+            final var methodsModifier = Modifier.toString(methods.getModifiers());
+            final var methodsName = methods.getName();
+            final var methodsReturnType = methods.getReturnType();
+            final var methodsParameter = methods.getParameters();
+
+            final var parameters = String.format("(%s)",
+                    Arrays.stream(methodsParameter).map(parameter -> String.format("%s %s", parameter.getType(), parameter.getName()))
+                            .collect(Collectors.joining(",")));
+
+            logger.info("{} {} {} {} ", methodsModifier, methodsReturnType, methodsName, parameters);
+
+        }
+
     }
-    
+
+
     @Test
     public void newInstanceWithConstructorArgs() {
         Class<User> clazz = User.class;
         logger.debug(clazz.getName());
     }
-    
+
     @Test
     public void privateFieldAccess() {
         Class<Student> clazz = Student.class;
