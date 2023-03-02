acf6c6787ed6a763c62b07d0613d01b62ed8692d
HiiWee <wpdlzhf159@gmail.com>
2023-03-02 21:39:37 +0900

    init: 10장 진행위한 필요 클래스 및 테스트 추가

---
+++ jwp-basic-gradle/src/main/java/core/nmvc/HandlerExecution.java
@@ -0,0 +1,11 @@
+package core.nmvc;
+
+import core.mvcframework.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HandlerExecution {
+    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return null;
+    }
+}
---
+++ jwp-basic-gradle/src/test/java/core/ref/Junit4TestRunner.java
@@ -0,0 +1,12 @@
+package core.ref;
+
+
+import org.junit.jupiter.api.Test;
+
+public class Junit4TestRunner {
+    @Test
+    public void run() throws Exception {
+        Class<Junit4Test> clazz = Junit4Test.class;
+
+    }
+}
