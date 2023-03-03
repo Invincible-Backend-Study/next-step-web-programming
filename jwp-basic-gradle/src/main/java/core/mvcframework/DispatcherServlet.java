a485858560d6b7dc5c3af2b9d6f2ee7a4e07ec58
HiiWee <wpdlzhf159@gmail.com>
2023-03-03 20:16:02 +0900

    refactor: 디스패처 서블릿에서 구현한 핸들러 매핑을 모두 동작하도록 변경

--- jwp-basic-gradle/src/main/java/core/mvcframework/DispatcherServlet.java
+++ jwp-basic-gradle/src/main/java/core/mvcframework/DispatcherServlet.java
@@ -1,8 +1,14 @@
 package core.mvcframework;
 
+import com.google.common.collect.Lists;
 import core.mvcframework.controller.Controller;
 import core.mvcframework.view.View;
+import core.nmvc.AnnotationHandlerMapping;
+import core.nmvc.HandlerExecution;
+import core.nmvc.HandlerMapping;
 import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,25 +22,43 @@ public class DispatcherServlet extends HttpServlet {
 
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private RequestMapping requestMapping;
+    private final List<HandlerMapping> handlerMappings = Lists.newArrayList();
 
     @Override
     public void init() {
-        requestMapping = new RequestMapping();
-        requestMapping.initMapping();
+        handlerMappings.add(new LegacyHandlerMapping());
+        handlerMappings.add(new AnnotationHandlerMapping("next"));
+        for (HandlerMapping handlerMapping : handlerMappings) {
+            handlerMapping.initialize();
+        }
     }
 
     @Override
     protected void service(final HttpServletRequest request, final HttpServletResponse response)
             throws ServletException, IOException {
-        Controller handler = (Controller) requestMapping.getHandler(request);
-        if (handler == null) {
-            throw new IllegalArgumentException("[ERROR] No URL mapping");
-        }
-        ModelAndView modelAndView = handler.execute(request, response);
+        ModelAndView modelAndView = getHandlerResult(request, response);
         View view = modelAndView.getView();
         log.debug("executeResultView={}", view);
         view.render(modelAndView.getModel(), request, response);
+    }
+
+    private ModelAndView getHandlerResult(final HttpServletRequest request, final HttpServletResponse response) {
+        Object handler = getHandler(request);
+        if (handler instanceof Controller) {
+            return ((Controller) handler).execute(request, response);
+        }
+        if (handler instanceof HandlerExecution) {
+            return ((HandlerExecution) handler).handle(request, response);
+        }
+        throw new IllegalArgumentException("Can not find execute handler");
+    }
+
+    private Object getHandler(final HttpServletRequest request) {
+        return handlerMappings.stream()
+                .map(handlerMapping -> handlerMapping.getHandler(request))
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("No URL Mapping"));
     }
 
 }
