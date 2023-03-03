de125daea0caedbc35943b4355d44eebb15ea8a0
HiiWee <wpdlzhf159@gmail.com>
2023-03-03 18:37:09 +0900

    fix: method invoke시 메소드가 포함된 클래스 전달하도록 수정

--- jwp-basic-gradle/src/main/java/core/nmvc/HandlerExecution.java
+++ jwp-basic-gradle/src/main/java/core/nmvc/HandlerExecution.java
@@ -1,6 +1,7 @@
 package core.nmvc;
 
 import core.mvcframework.ModelAndView;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,8 +12,16 @@ public class HandlerExecution {
     private final Method method;
 
     public HandlerExecution(final Method method) {
-        this.declaredObject = method.getDeclaringClass();
+        this.declaredObject = getDeclaredObject(method);
         this.method = method;
+    }
+
+    private static Object getDeclaredObject(final Method method) {
+        try {
+            return method.getDeclaringClass().getConstructor().newInstance();
+        } catch (Exception exception) {
+            throw new RuntimeException(exception);
+        }
     }
 
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
