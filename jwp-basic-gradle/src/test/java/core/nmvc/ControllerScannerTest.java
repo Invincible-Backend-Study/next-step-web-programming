5d524899220c0161173ba7da76eb32d353233eb4
HiiWee <wpdlzhf159@gmail.com>
2023-03-04 00:03:40 +0900

    test: ControllerScanner 테스트 추가

--- jwp-basic-gradle/src/test/java/core/nmvc/ControllerScannerTest.java
+++ jwp-basic-gradle/src/test/java/core/nmvc/ControllerScannerTest.java
@@ -1,61 +1,37 @@
 package core.nmvc;
 
-import com.google.common.collect.Maps;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import core.annotation.Controller;
-import core.annotation.RequestMapping;
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.List;
+import core.ref.Student;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 
 public class ControllerScannerTest {
-//    ControllerScanner controllerScanner = new ControllerScanner();
 
-    Map<Class<?>, Object> controllers = Maps.newHashMap();
-    private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
-
+    ControllerScanner controllerScanner;
 
     @BeforeEach
-    @SuppressWarnings("unchecked")
-    void test() {
-        Reflections reflections = new Reflections("next.controller.qna", "next.controller.user");
-        Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(Controller.class);
-        System.out.println(annotated);
-        annotated.forEach(clazz -> {
-            try {
-                controllers.put(clazz, clazz.getConstructor().newInstance());
-            } catch (Exception exception) {
-                throw new RuntimeException(exception);
-            }
-        });
-        System.out.println(controllers);
+    void setup() {
+        controllerScanner = new ControllerScanner("core.nmvc");
     }
 
-    /**
-     * 찾은 메소드에서 url 정보와 requestMethod 정보를 찾아 HandlerKey를 만든다.
-     */
     @Test
-    void test2() {
-        Set<Class<?>> clazzes = controllers.keySet();
-        for (Class<?> clazz : clazzes) {
-            Method[] declaredMethods = clazz.getDeclaredMethods();
-            List<Method> methods = Arrays.stream(declaredMethods)
-                    .filter(method -> method.isAnnotationPresent(RequestMapping.class))
-                    .collect(Collectors.toList());
-            methods.forEach(method -> {
-                RequestMapping declaredAnnotation = method.getDeclaredAnnotation(RequestMapping.class);
-                HandlerKey handlerKey = new HandlerKey(declaredAnnotation.value(), declaredAnnotation.method());
-                HandlerExecution handlerExecution = new HandlerExecution(method);
-                handlerExecutions.put(handlerKey, handlerExecution);
-            });
+    @DisplayName("MyController 스캔 테스트")
+    void getControllers() {
+        // when
+        Map<Class<?>, Object> controllers = controllerScanner.getControllers();
 
-        }
+        // then
+        Assertions.assertAll(
+                () -> assertThat(controllers.size()).isEqualTo(1),
+                () -> assertThat(controllers.get(MyController.class).toString().contains("core.nmvc.MyController")).isTrue()
+        );
 
-        System.out.println(handlerExecutions);
     }
 }
