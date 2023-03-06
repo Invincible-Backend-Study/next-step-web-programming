f9de0a6ef6699673538d43c733eb9d9dcc5dbbe4
JaehongDev <println@kakao.com>
2023-03-06 10:27:56 +0900

    feature: controller adapter 구현

--- jwp-basic-gradle/src/main/java/core/rc/ControllerHandlerAdapter.java
+++ jwp-basic-gradle/src/main/java/core/rc/ControllerHandlerAdapter.java
@@ -1,2 +1,18 @@
-package core.rc;public class ControllerHandlerAdapter {
+package core.rc;
+
+import core.mvc.Controller;
+import core.mvc.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ControllerHandlerAdapter implements HandlerAdapter {
+    @Override
+    public boolean supports(Object handler) {
+        return handler instanceof Controller;
+    }
+
+    @Override
+    public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handle) throws Exception {
+        return ((Controller) handle).execute(request, response);
+    }
 }
