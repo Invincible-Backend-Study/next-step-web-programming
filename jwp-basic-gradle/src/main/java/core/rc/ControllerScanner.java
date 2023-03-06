4a5bc4a83c1fe9d82e273a91cebe56a22555f7ea
JaehongDev <println@kakao.com>
2023-03-06 10:27:41 +0900

    refactor: controller scanner 리팩터리

--- jwp-basic-gradle/src/main/java/core/rc/ControllerScanner.java
+++ jwp-basic-gradle/src/main/java/core/rc/ControllerScanner.java
@@ -1,11 +1,9 @@
 package core.rc;
 
-import com.google.common.collect.Maps;
 import core.annotation.Controller;
 import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.reflections.Reflections;
 
@@ -20,33 +18,17 @@ public class ControllerScanner {
     }
 
     public Map<Class<?>, Object> getControllers() {
-        final var preInitiatedControllers = reflections.getTypesAnnotatedWith(Controller.class);
-        return instantiateControllers(preInitiatedControllers);
+        return reflections.getTypesAnnotatedWith(Controller.class)
+                .stream()
+                .collect(Collectors.toMap(clazz -> clazz, this::convertClazzToInstance));
     }
 
-    private Map<Class<?>, Object> instantiateControllers(Set<Class<?>> preInitiatedControllers) {
-        final Map<Class<?>, Object> controllers = Maps.newHashMap();
+    private Object convertClazzToInstance(Class<?> clazz) {
         try {
-            for (Class<?> clazz : preInitiatedControllers) {
-                controllers.put(clazz, clazz.getDeclaredConstructor().newInstance());
-            }
-        } catch (InvocationTargetException | InstantiationException | IllegalAccessException | NoSuchMethodException e) {
+            return clazz.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            log.error("{}", e.getMessage());
             throw new RuntimeException(e);
         }
-
-        return controllers;
-    }
-
-    public void scan() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        final var reflections = new Reflections("core.rc");
-
-        // 컨트롤러 어노테이션을 찾음
-        final var annotated = reflections.getTypesAnnotatedWith(Controller.class);
-
-        final var mapping = new HashMap<Class<?>, Object>();
-        for (var clazz : annotated) {
-            final var instance = clazz.getDeclaredConstructor().newInstance();
-        }
-        log.info("{}", annotated);
     }
 }
