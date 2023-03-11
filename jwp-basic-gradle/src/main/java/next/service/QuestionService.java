1bcca0cdf06f51b304dd5b8f26b559d3f671a602
wnsgh12s <junho1273@gmail.com>
2023-03-11 18:10:10 +0900

    refactor : 컨트롤러의  비즈니스 로직을 service 클래스가 담당하도록 변경

--- jwp-basic-gradle/src/main/java/next/service/QuestionService.java
+++ jwp-basic-gradle/src/main/java/next/service/QuestionService.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 import next.dao.AnswerDao;
 import next.dao.QuestionDao;
 import next.model.Answer;
+import next.model.Form;
 import next.model.Question;
 import next.model.User;
 
@@ -13,20 +14,10 @@ public class QuestionService {
     private final QuestionDao questionDao = QuestionDao.getInstance();
     private final AnswerDao answerDao = AnswerDao.getInstance();
 
+
+
     public Question findByQuestionId(int questionId) {
         return questionDao.findByQuestionId(questionId);
-    }
-
-    public boolean deleteQuestion(User user, Question question) {
-        List<Answer> answers = answerDao.findAllByQuestonId(question.getQuestionId());
-        boolean canDelete = answers.stream().allMatch(answer -> Objects.equals(answer.getWriter(), user.getName()));
-        if (canDelete) {
-            questionDao.deleteAnswer(question.getQuestionId());
-            answers.forEach(answer -> answerDao.deleteAnswer(answer.getAnswerId()));
-            return true;
-        } else {
-            return false;
-        }
     }
 
     public List<Question> findAll() {
@@ -37,7 +28,45 @@ public class QuestionService {
         questionDao.addQuestion(question);
     }
 
-    public void updateQuestion(String contents, String title, String id) {
-        questionDao.update(contents, title, id);
+    public void updateQuestion(Form form, User user) throws Exception {
+        validateUpdate(form, user);
+        questionDao.update(form.getContents(), form.getTitle(), form.getId());
+    }
+
+    private void validateUpdate(Form form, User user) throws Exception {
+        if (user == null) {
+            throw new Exception("로그인 하지 않았습니다.");
+        }
+        Question question = questionDao.findByQuestionId(Integer.parseInt(form.getId()));
+        if (!Objects.equals(user.getName(), question.getWriter())) {
+            throw new Exception("작성자가 아닐시 삭제할 수 없습니다.");
+        }
+    }
+
+    public boolean deleteQuestion(int questionId, User user) throws Exception {
+        Question question = questionDao.findByQuestionId(questionId);
+        validateDelete(user, question);
+        List<Answer> answers = answerDao.findAllByQuestonId(question.getQuestionId());
+        boolean canDelete = answers.stream().allMatch(answer -> Objects.equals(answer.getWriter(), user.getName()));
+        if (canDelete) {
+            questionDao.deleteAnswer(question.getQuestionId());
+            answers.forEach(answer -> answerDao.deleteAnswer(answer.getAnswerId()));
+            return true;
+        }
+        return false;
+    }
+
+    private void validateDelete(User user, Question question) throws Exception {
+        if (question == null) {
+            throw new Exception("해당 게시물이 존재하지 않습니다.");
+        }
+
+        if (user == null) {
+            throw new Exception("삭제하려면 로그인 하셔야합니다.");
+        }
+
+        if (!Objects.equals(user.getName(), question.getWriter())) {
+            throw new Exception("삭제하려면 로그인 하셔야합니다.");
+        }
     }
 }
