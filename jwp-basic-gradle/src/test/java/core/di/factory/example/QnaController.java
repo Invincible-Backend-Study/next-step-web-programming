671108c82adb590c6c0538c8fc34540bb909f3c6
HiiWee <wpdlzhf159@gmail.com>
2023-03-12 00:42:06 +0900

    init: di 프레임워크 구현 초기 제공 코드 추가

---
+++ jwp-basic-gradle/src/test/java/core/di/factory/example/QnaController.java
@@ -0,0 +1,29 @@
+package core.di.factory.example;
+
+import core.mvcframework.ModelAndView;
+import core.mvcframework.controller.AbstractAnnotationController;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import core.annotation.Controller;
+import core.annotation.Inject;
+import core.annotation.RequestMapping;
+
+@Controller
+public class QnaController extends AbstractAnnotationController {
+    private MyQnaService qnaService;
+
+    @Inject
+    public QnaController(MyQnaService qnaService) {
+        this.qnaService = qnaService;
+    }
+
+    public MyQnaService getQnaService() {
+        return qnaService;
+    }
+
+    @RequestMapping("/questions")
+    public ModelAndView list(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return jspView("/qna/list.jsp");
+    }
+}
