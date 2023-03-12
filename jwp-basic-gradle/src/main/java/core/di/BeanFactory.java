a6888fbf999c794a529b46057f6923b94ebbf393
HiiWee <wpdlzhf159@gmail.com>
2023-03-12 21:27:56 +0900

    feat(BeanFactory): 애노테이션 기반 스캔 및 의존성 주입 기능 추가

--- jwp-basic-gradle/src/main/java/core/di/BeanFactory.java
+++ jwp-basic-gradle/src/main/java/core/di/BeanFactory.java
@@ -1,22 +1,26 @@
 package core.di;
 
-import java.util.Map;
-import java.util.Set;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static core.di.BeanFactoryUtils.findConcreteClass;
+import static core.di.BeanFactoryUtils.getInjectedConstructor;
 
 import com.google.common.collect.Maps;
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 
 public class BeanFactory {
-    private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
+    private static final Logger log = LoggerFactory.getLogger(BeanFactory.class);
 
-    private Set<Class<?>> preInstanticateBeans;
+    private Set<Class<?>> preInstantiatedBeans;
 
     private Map<Class<?>, Object> beans = Maps.newHashMap();
 
-    public BeanFactory(Set<Class<?>> preInstanticateBeans) {
-        this.preInstanticateBeans = preInstanticateBeans;
+    public BeanFactory(Set<Class<?>> preInstantiatedBeans) {
+        this.preInstantiatedBeans = preInstantiatedBeans;
     }
 
     @SuppressWarnings("unchecked")
@@ -25,6 +29,43 @@ public class BeanFactory {
     }
 
     public void initialize() {
-
+        for (Class<?> preInstantiatedBean : preInstantiatedBeans) {
+            if (beans.containsKey(preInstantiatedBean)) {
+                continue;
+            }
+            instantiateClass(preInstantiatedBean);
+        }
+        log.debug("complete bean scan");
     }
+
+    private void instantiateClass(final Class<?> preInstantiatedBean) {
+        Constructor<?> injectedConstructor = getInjectedConstructor(preInstantiatedBean);
+        if (injectedConstructor == null) {
+            Object bean = BeanUtils.instantiateClass(findConcreteClass(preInstantiatedBean, preInstantiatedBeans));
+            beans.put(preInstantiatedBean, bean);
+            return;
+        }
+        beans.put(preInstantiatedBean, instantiateParameterConstructor(injectedConstructor));
+    }
+
+    private Object instantiateParameterConstructor(final Constructor<?> injectedConstructor) {
+        Class<?>[] parameterTypes = injectedConstructor.getParameterTypes();
+        instantiateParameter(parameterTypes);
+        return BeanUtils.instantiateClass(injectedConstructor, findBeans(parameterTypes));
+    }
+
+    private void instantiateParameter(final Class<?>[] parameterTypes) {
+        for (Class<?> parameterType : parameterTypes) {
+            if (!beans.containsKey(parameterType)) {
+                instantiateClass(parameterType);
+            }
+        }
+    }
+
+    private Object[] findBeans(final Class<?>[] parameterTypes) {
+        return Arrays.stream(parameterTypes)
+                .map(parameterType -> beans.get(parameterType))
+                .toArray();
+    }
+
 }
--- jwp-basic-gradle/src/main/java/core/di/BeanFactoryUtils.java
+++ jwp-basic-gradle/src/main/java/core/di/BeanFactoryUtils.java
@@ -3,22 +3,20 @@ package core.di;
 import static org.reflections.ReflectionUtils.getAllConstructors;
 import static org.reflections.ReflectionUtils.withAnnotation;
 
+import com.google.common.collect.Sets;
+import core.annotation.Inject;
 import java.lang.reflect.Constructor;
 import java.util.Set;
-
-import com.google.common.collect.Sets;
-
-import core.annotation.Inject;
 
 public class BeanFactoryUtils {
     /**
      * 인자로 전달하는 클래스의 생성자 중 @Inject 애노테이션이 설정되어 있는 생성자를 반환
-     * 
-     * @Inject 애노테이션이 설정되어 있는 생성자는 클래스당 하나로 가정한다.
+     *
      * @param clazz
      * @return
+     * @Inject 애노테이션이 설정되어 있는 생성자는 클래스당 하나로 가정한다.
      */
-    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @SuppressWarnings({"rawtypes", "unchecked"})
     public static Constructor<?> getInjectedConstructor(Class<?> clazz) {
         Set<Constructor> injectedConstructors = getAllConstructors(clazz, withAnnotation(Inject.class));
         if (injectedConstructors.isEmpty()) {
@@ -28,9 +26,9 @@ public class BeanFactoryUtils {
     }
 
     /**
-     * 인자로 전달되는 클래스의 구현 클래스. 만약 인자로 전달되는 Class가 인터페이스가 아니면 전달되는 인자가 구현 클래스,
-     * 인터페이스인 경우 BeanFactory가 관리하는 모든 클래스 중에 인터페이스를 구현하는 클래스를 찾아 반환
-     * 
+     * 인자로 전달되는 클래스의 구현 클래스. 만약 인자로 전달되는 Class가 인터페이스가 아니면 전달되는 인자가 구현 클래스, 인터페이스인 경우 BeanFactory가 관리하는 모든 클래스 중에 인터페이스를
+     * 구현하는 클래스를 찾아 반환
+     *
      * @param injectedClazz
      * @param preInstanticateBeans
      * @return
