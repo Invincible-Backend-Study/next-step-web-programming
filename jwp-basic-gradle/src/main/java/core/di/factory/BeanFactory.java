97f8b7f16adbef4c7ac32479d40c5845e27181f1
wnsgh12s <junho1273@gmail.com>
2023-03-16 11:28:51 +0900

    feat: 클래스를 가공해서 bean을 생성하는 beanFactory 클래스 생성

--- jwp-basic-gradle/src/main/java/core/di/factory/BeanFactory.java
+++ jwp-basic-gradle/src/main/java/core/di/factory/BeanFactory.java
@@ -1,12 +1,20 @@
 package core.di.factory;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import core.annotation.Controller;
+import core.annotation.Inject;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.google.common.collect.Maps;
+import org.springframework.beans.BeanInstantiationException;
+import org.springframework.beans.BeanUtils;
 
 public class BeanFactory {
     private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
@@ -24,7 +32,80 @@ public class BeanFactory {
         return (T) beans.get(requiredType);
     }
 
-    public void initialize() {
+    private Object instantiateClass(Class<?> clazz) {
+        Object bean = beans.get(clazz);
+        if (bean != null) {
+            return bean;
+        }
+        Constructor<?> injectConstructor = getConstructorsContainingInjects(clazz);
+        if (injectConstructor == null) {
+            bean = instantiate(clazz);
+            beans.put(clazz, bean);
+            return bean;
+        }
+        bean = instantiateConstructor(injectConstructor);
+        beans.put(clazz, bean);
+        return bean;
+    }
 
+    private Object instantiate(Class<?> clazz) {
+        if (clazz.isInterface()) {
+            throw new BeanInstantiationException(clazz, "인터페이스는 안돼요 안됑.");
+        }
+        try {
+            return clazz.getConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Constructor<?> getConstructorsContainingInjects(Class<?> clazz) {
+        for (Constructor<?> constructor : clazz.getConstructors()) {
+            boolean isContainInject = Arrays.stream(constructor.getAnnotations())
+                    .anyMatch(annotation -> annotation.annotationType().equals(Inject.class));
+            if (isContainInject) {
+                return constructor;
+            }
+        }
+        return null;
+    }
+
+    private Object instantiateConstructor(Constructor<?> constructor) {
+        //생성자의 파라미터 싹다 가져와!!
+        Class<?>[] pTypes = constructor.getParameterTypes();
+        List<Object> args = Lists.newArrayList();
+        for (Class<?> clazz : pTypes) {
+            Class<?> concreteClass = BeanFactoryUtils.findConcreteClass(clazz, preInstanticateBeans);
+
+            if (!preInstanticateBeans.contains(concreteClass)) {
+                throw new IllegalStateException(clazz + "는 빈이 아니다.");
+            }
+            Object bean = beans.get(concreteClass);
+            if (bean == null) {
+                bean = instantiateClass(concreteClass);
+            }
+            args.add(bean);
+        }
+        return BeanUtils.instantiateClass(constructor, args.toArray());
+    }
+
+    public void initialize() {
+        for (Class<?> clazz : preInstanticateBeans) {
+            if (beans.get(clazz) == null) {
+                instantiateClass(clazz);
+            }
+        }
+    }
+
+    public Map<Class<?>, Object> getControllers() {
+        Map<Class<?>, Object> controllers = new HashMap<>();
+        for (Class<?> clazz : preInstanticateBeans) {
+            Controller annotation = clazz.getAnnotation(Controller.class);
+            if (annotation != null) {
+                controllers.put(clazz, getBean(clazz));
+            }
+        }
+        return controllers;
     }
 }
